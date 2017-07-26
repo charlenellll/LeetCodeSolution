6d0d82d3401e8281716e5c255d101d1a3f3efe03
Jialing Liao <charleneliaojl@gmail.com>
2017-07-26 16:19:34 +0800

    Map

--- 350 Intersection of Two Arrays II/intersectionII.cpp
+++ 350 Intersection of Two Arrays II/intersectionII.cpp
@@ -1,3 +1,7 @@
+// The underlying mechanism for set and map in C++ is balanced binary tree
+// Time complexity: O(nlogn)
+// Space complexity: O(n)
+
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
@@ -17,6 +21,7 @@ public:
 
 
         // Some changes to avoid initial value problems in the map if we are not so confident about one language
+        // O(nlogn)
         map<int,int> record;
         for(int i = 0; i < nums1.size(); i++){
             if( record.find[nums1[i]] == record.end())
@@ -25,6 +30,7 @@ public:
                 record[nums1[i]]++;
         }
         
+        // O(nlogn)
         vector<int> resultVector;
         for(int i = 0; i < nums2.size(); i++)
             if( record.find(nums2[i]) != record.end() && record[nums2[i]] > 0 ){
