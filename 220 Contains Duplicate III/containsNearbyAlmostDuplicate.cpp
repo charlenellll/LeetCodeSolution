fdccabd9fd01e7adb03c53a94941a4c9928a527e
Jialing Liao <charleneliaojl@gmail.com>
2017-07-27 22:49:48 +0800

    220# Only in C++. No Java & Python yet.

---
+++ 220 Contains Duplicate III/containsNearbyAlmostDuplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        
+        set<long long> record;
+        for(int i = 0; i < nums.size(); i++){
+            
+            if( record.lower_bound((long long)nums[i] - (long long)t) != record.end() && *record.lower_bound((long long)nums[i] - (long long)t) <= (long long)nums[i] + (long long)t )
+                return true;
+            
+            record.insert(nums[i]);
+            if( record.size() == k+1 )
+                record.erase( nums[i-k] );
+        }
+        return false;
+    }
+};
