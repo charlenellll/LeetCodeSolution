9243fe723b7a841a96cbc39aea69efcf8358510c
Jialing Liao <charleneliaojl@gmail.com>
2017-08-01 17:18:03 +0800

    94# and 145# in C++. No Java or python yet.

---
+++ 145 Binary Tree Postorder Traversal(C++ only)/postorderTraversal.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+struct Command{
+    string s; // go, print
+    TreeNode* node;
+    Command( string s, TreeNode* node): s(s), node(node){}
+};
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+
+        vector<int> res;
+        if( root == NULL )
+            return res;
+        
+        stack<Command> stack;
+        stack.push( Command("go", root) );
+        while( !stack.empty() ){
+            
+            Command command = stack.top();
+            stack.pop();
+            
+            if( command.s == "print" )
+                res.push_back( command.node -> val );
+            else{
+                assert( command.s == "go" );
+                stack.push( Command("print",command.node) );
+                if( command.node-> right )
+                    stack.push( Command("go",command.node->right) );
+                if( command.node-> left )
+                    stack.push( Command("go",command.node->left) );
+            }
+        }
+        
+        return res;
+    }
+};
